f63876aa33c1c766e032ab9faa4ec7c3c2a160ab
Dan Manning <dmanning23@hotmail.com>
2016-04-24 20:07:31 -0500

    moved into pcl, added winuni

--- TouchScreenBuddy/TouchScreenBuddy.WindowsUniversal/Properties/AssemblyInfo.cs
+++ TouchScreenBuddy/TouchScreenBuddy.WindowsUniversal/Properties/AssemblyInfo.cs
@@ -5,22 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TouchScreenBuddy")]
+[assembly: AssemblyTitle("TouchScreenBuddy.WindowsUniversal")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TouchScreenBuddy")]
+[assembly: AssemblyProduct("TouchScreenBuddy.WindowsUniversal")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fb2eaa12-94c2-4453-8b04-790e5ac53e73")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,3 +26,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
--- TouchScreenBuddy/buildNugetPackages.ps1
+++ TouchScreenBuddy/buildNugetPackages.ps1
@@ -1,3 +1,2 @@
-nuget pack .\TouchScreenBuddy.Android\TouchScreenBuddy.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\TouchScreenBuddy.iOS\TouchScreenBuddy.iOS.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-nuget pack .\TouchScreenBuddy.DesktopGL\TouchScreenBuddy.DesktopGL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\TouchScreenBuddy.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget push *.nupkg
